8eaae533a8c8622b5e3ec6fdb4416226d0128b30
belpythons <zerobocilicung393@gmail.com>
2026-06-05 21:33:08 +0800

    feat(products): update product model definition and regenerate migrations

--- products/models.py
+++ products/models.py
@@ -4,9 +4,16 @@ from django.db import models
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    sku = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    location = models.CharField(max_length=255, blank=True, null=True)
+    shop = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    price = models.IntegerField()
+    discount = models.IntegerField(default=0)
+    category = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    picture = models.URLField(max_length=500, blank=True, null=True)
     is_delete = models.BooleanField(default=False)
 
     def __str__(self):
